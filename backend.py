41225fa8375a0b26b0e70326035feb83fcef0e57
sadjeda <sadjeda.benhamed@ensia.edu.dz>
2025-10-30 10:15:31 +0100

    Fix port and host for Render deployment

--- backend.py
+++ backend.py
@@ -34,6 +34,6 @@ def index():
 	return 'Welcome ENSIA Students from Flask!'
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
